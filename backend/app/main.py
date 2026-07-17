03d129b4e0e4d5b599bbb60818342932232308e4
Aswinbabu5 <ashwinbabu769@gmail.com>
2026-07-17 15:59:02 +0530

    final link

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,8 +13,8 @@ app = FastAPI(
 
 origins = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
-    "https://ai-first-crm-hcp-nlb1.vercel.app"
+    "http://127.0.0.1:5173",
+    "https://ai-first-crm-hcp-mauve.vercel.app"
 ]
 
 app.add_middleware(
